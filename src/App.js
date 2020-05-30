aa1c887d53ac81251e386cbd565da2d3832e1726
Arunabho Majumder <Arunabho@MacBook-Air.local>
2020-05-31 03:59:47 +0530

    Working with list and conditionals

--- src/App.js
+++ src/App.js
@@ -23,15 +23,22 @@ class App extends Component {
   //     ],
   //   });
   // };
-  // nameChangeHandler = (event) => {
-  //   this.setState({
-  //     person: [
-  //       { name: "Max", age: 28 },
-  //       { name: event.target.value, age: 29 },
-  //       { name: "Stephanie", age: 26 },
-  //     ],
-  //   });
-  // };
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return (p.id = id);
+    });
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  };
+
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
@@ -60,7 +67,8 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={persons.name}
                 age={persons.age}
-                id={persons.id}
+                key={persons.id}
+                changed={(event) => this.nameChangeHandler(event, persons.id)}
               />
             );
           })}
