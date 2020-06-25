766bc72903b96200bcbb766b4b58d2103d668a7e
Arunabho Majumder <Arunabho@MacBook-Air.local>
2020-06-25 10:21:14 +0530

    Styling react components #css Module

--- src/App.js
+++ src/App.js
@@ -51,10 +51,11 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
+      cursor: "pointer",
     };
     let persons = null;
 
@@ -74,11 +75,28 @@ class App extends Component {
           })}
         </div>
       );
+      // style.backgroundColor = "red";
+      style.backgroundColor = "red";
+    }
+    //let classes = ["red", "bold"].join(" ");
+    // const classes = [];
+    // if (this.state.persons.length <= 3) {
+    //   classes.push("red");
+    // }
+    // if (this.state.persons.length <= 1) {
+    //   classes.push("bold");
+    // }
+    const classes = [];
+    if (this.state.persons.length <= 3) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
     return (
       <div className='App'>
         <h1>Hi, I'm a React Developer.</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(" ")}>This is really working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
